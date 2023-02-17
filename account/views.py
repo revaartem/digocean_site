3d82bcafdc544c948fcdb31ef754e9f56893ef91
Reva Artem <113471817+revaartem@users.noreply.github.com>
2023-02-17 19:14:03 +0200

    Update views.py

--- account/views.py
+++ account/views.py
@@ -5,6 +5,19 @@ from django.contrib.auth import login, authenticate, logout
 
 
 def registration_view(request):
+    """
+    View function of the registration page. Processed POST and GET requests.
+    
+    form - Registration form.\n
+    information_in_contact_us - Object model InformationInContactUs.\n
+    footer - Footer object model.\n
+    user_manager - Checked if user have group 'manager' in his group list.\n
+    user_auth - Is user authenticated or not.\n
+
+    :param request: POST or GET request.
+    :return: Render of the HTML-page with context.
+    
+    """
     form = UserRegistration(request.POST or None)
     information_in_contact_us = InformationInContactUs.objects.get()
     footer = Footer.objects.get()
@@ -31,6 +44,19 @@ def registration_view(request):
 
 
 def login_view(request):
+    """
+    View function of the login page. Processed POST and GET requests.
+    
+    form - form from model UserLogin with GET request or None.\n
+    next_get - Next page parameters.\n
+    information_in_contact_us - Object model InformationInContactUs.\n
+    footer - footer - Footer object model.\n
+    user_manager - Checked if user have group 'manager' in his group list.\n
+    user_auth - Is user authenticated or not.\n
+    
+    :param request: POST or GET request.
+    :return: Render of the HTML-page with context.
+    """
     form = UserLogin(request.POST or None)
     next_get = request.GET.get('next')
     information_in_contact_us = InformationInContactUs.objects.get()
@@ -58,5 +84,11 @@ def login_view(request):
 
 
 def logout_view(request):
+    """
+    Logout view.
+    
+    :param request: GET request.
+    :return: Redirect to the main page.
+    """
     logout(request)
     return redirect('/')
